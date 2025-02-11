5a67f8cde43281a71beae13e029c07539f13f332
Ayush Priyadarshi <ayushpriye2105@gmail.com>
2025-02-11 15:49:10 +0530

    Resolved Firebase connectivity Problems

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,6 @@ import Login from './login';
 import SignInScreen from './signin';
 import Signup from './signup';
 import auth from '@react-native-firebase/auth';
-import firebase from '@react-native-firebase/app';
-
-// Firebase Configuration (Only if needed, otherwise remove)
-const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-};
-
-// Ensure Firebase is initialized
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
 
 // Stack Navigator
 const Stack = createStackNavigator();
