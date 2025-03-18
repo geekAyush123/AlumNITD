5f63f779f6c0392d11e85807d97fc2b03887b27e
Ayush Priyadarshi <ayushpriye2105@gmail.com>
2025-03-19 01:35:07 +0530

    Map done..

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "react-native-gesture-handler"; 
+import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -8,6 +8,7 @@ import HomePage from "./HomePage";
 import ProfileScreen from "./Profile";
 import AlumniSearchScreen from "./AlumniSearchScreen";
 import ProfilePage from "./ProfilePage";
+import AlumniMapScreen from "./AlumniMapScreen";
 
 const Stack = createStackNavigator();
 
@@ -18,17 +19,18 @@ function App() {
         <Stack.Screen
           name="Login"
           component={LoginScreen}
-          options={{ headerShown: false }} 
+          options={{ headerShown: false }}
         />
         <Stack.Screen
           name="Register"
           component={RegisterScreen}
-          options={{ headerShown: true, title: "Back" }} 
+          options={{ headerShown: true, title: "Back" }}
         />
         <Stack.Screen name="Home" component={HomePage} options={{ headerShown: false }} />
         <Stack.Screen name="ProfilePage" component={ProfilePage} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="AlumniSearch" component={AlumniSearchScreen} options={{ title: "Search Alumni" }} /> 
+        <Stack.Screen name="AlumniSearch" component={AlumniSearchScreen} options={{ title: "Search Alumni" }} />
+        <Stack.Screen name="AlumniMapScreen" component={AlumniMapScreen} options={{ title: "Alumni Map" }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
